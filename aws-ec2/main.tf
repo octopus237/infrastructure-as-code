bf4b587ff7b795fbb7a9ebf51d89b6a49fccfd9e
st9 <briceinno@gmail.com>
2023-06-25 17:26:08 +0300

    update

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,39 +1,39 @@
 resource "aws_instance" "app_server" {
-  ami           = var.ami
-  instance_type = var.instance_type 
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.ssh-access.id]
-  key_name = "${aws_key_pair.dev-server.key_name}"
+  key_name               = aws_key_pair.dev-server.key_name
 
   tags = {
-    Name = "Dev-Server"
+    Name        = "Dev-Server"
     Environment = "Dev"
   }
 }
 
 resource "aws_key_pair" "dev-server" {
   public_key = file(var.public_key)
-  key_name = "server-key"
+  key_name   = "server-key"
 }
 
 resource "aws_security_group" "ssh-access" {
-  name = "ssh-access"
+  name        = "ssh-access"
   description = "Allow ssh from internet"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
 
-output publicip {
+output "publicip" {
   value = aws_instance.app_server.public_ip
 }
--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
-    default = "us-west-2"
+  default = "us-west-2"
 }
 
 variable "instance_type" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "ami" {
-    default = "ami-07dfed28fcf95241c"
+  default = "ami-07dfed28fcf95241c"
 }
 
 variable "public_key" {
